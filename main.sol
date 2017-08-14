c84e5aec0e35f1d5d7bea861d9cbf4869fa867eb
Postables <alextrottier2014@gmail.com>
2017-08-13 22:34:21 -0700

    Added safe math contract

--- main.sol
+++ main.sol
@@ -1,6 +1,32 @@
-pragma solidity ^0.4.14;
+pragma solidity ^0.4.11;
 
-contract IpfsControls {
+contract SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract IpfsControls is SafeMath {
 
     struct IpfsEntries {
         string ipfsHash;
@@ -40,7 +66,9 @@ contract IpfsControls {
     }
 
     function getFile(string _dataName) constant returns (bytes32) {
-        return ipfsEntries[_dataName];
+        // Eventually a searchable index will be made that can be used to view a list of hashes
+        // This will then be expanded to keyword based categorization
+        return ipfsEntryTracker[_dataName];
     }
 
  
