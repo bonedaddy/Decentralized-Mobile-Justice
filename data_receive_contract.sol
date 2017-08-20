cb2f25c15c681273d59ca0dff2202e5c910264f1
Postables <alextrottier2014@gmail.com>
2017-08-20 07:47:57 -0700

    commmits

--- data_receive_contract.sol
+++ data_receive_contract.sol
@@ -20,9 +20,56 @@ contract Owner {
 }
 contract DataReceive is Owner {
 
-    mapping (string => string) public ipfsTracker;
+
+
+
+    // used to keep track of entries. Each IPFS hash when entered is set to true
+    // This is used so that once an ipfsHash has been entered it can't be changed 
     mapping (string => bool) public ipfsHashEntered;
 
+    // tracks number of contributions for a given identity
+    mapping (address => uint256) public contributionTracker;
+
+    // used to check if a user has uploaded before
+    mapping (address => bool) public hasUploaded;
+
+    struct Entry {
+        uint256 id;
+        string hash;
+    }
+
+    // testing struct to track user entries
+    struct Uploaders {
+        address person;
+        uint256 numContributions;
+        // unused for now, will be used to track credits;
+        uint256 credits;
+        // each time  a user makes an upload, the key gets upped by 1, with a new value of the ipfs hash
+        mapping (uint256 => Entry) contributions;
+    }
+    // Used to keep trtack of  each uploader, how many videos they uploded, how many credits, etc...
+    mapping (address => Uploaders) public uploaderTracker;
+    
+    // this function can only be called by the contract when a user hasn't uploaded before
+    function initializeFirstEntry() private {
+        
+    }
+
+    function addEntry(address _recorder, string _ipfsHash, string _ipfsChecksum) external {
+        if(!hasUploaded) {
+            initializeFirstEntry(_recorder);
+        }
+        require(!ipfsHashEntered[_ipfsHash]);
+        require(_recorder != msg.sender);
+        contributionTracker[_recorder] += 1;        
+        ipfsTracker[_ipfsHash] = _ipfsChecksum;
+        ipfsHashEntered[_ipfsHash] = true;
+        DataEntry(_recorder, _ipfsHash, _ipfsChecksum);
+    }
+
+
+    // Event to log data entry
+    event DataEntry(address indexed _recorder, string indexed _ipfsHash, string indexed _ipfsChecksum);
 
     function DataReceive(string _ipfsHash, string _ipfsChecksum) {
         ipfsTracker[_ipfsHash] = _ipfsChecksum;
@@ -33,10 +80,18 @@ contract DataReceive is Owner {
         return ipfsTracker[_ipfsHash];
     }
 
-    function addEntry(string _ipfsHash, string _ipfsChecksum) external {
-        require(!ipfsHashEntered[_ipfsHash]);
-        ipfsTracker[_ipfsHash] = _ipfsChecksum;
-        ipfsHashEntered[_ipfsHash] = true;
+    function getIpfsHash()
+    function getContributionCount(address _person) constant returns (uint256 _amount) {
+        if(contributionTracker[_person] <= 0) {
+            return 0;
+        } else {
+            return contributionTracker[_person];    
+        }
     }
+
+
  
+    function verifiyDataIntegrity() constant returns (bool success) {
+        return true;
+    }
  }
