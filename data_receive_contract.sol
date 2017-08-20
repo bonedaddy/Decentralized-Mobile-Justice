95678ed313dd18535223f58161d5ac5132e86aef
Postables <alextrottier2014@gmail.com>
2017-08-20 03:16:06 -0700

    Very basic solidity code to add a value, and retrieve a value
    Very basic solidity code to add a value, and retrieve a value

--- data_receive_contract.sol
+++ data_receive_contract.sol
@@ -20,9 +20,23 @@ contract Owner {
 }
 contract DataReceive is Owner {
 
-    mapping (uint256 => bytes32) public ipfsTracker;
+    mapping (string => string) public ipfsTracker;
+    mapping (string => bool) public ipfsHashEntered;
 
-    function DataReceive(uint256 _someidentifier, string _ipfsHash) {
-        ipfsTracker[_someidentifier] = sha3(_ipfsHash);
+
+    function DataReceive(string _ipfsHash, string _ipfsChecksum) {
+        ipfsTracker[_ipfsHash] = _ipfsChecksum;
+        ipfsHashEntered[_ipfsHash] = true;
     }
-}
+
+    function getIpfsHashChecksum(string _ipfsHash) constant returns (string _checksum) {
+        return ipfsTracker[_ipfsHash];
+    }
+
+    function addEntry(string _ipfsHash, string _ipfsChecksum) external {
+        require(!ipfsHashEntered[_ipfsHash]);
+        ipfsTracker[_ipfsHash] = _ipfsChecksum;
+        ipfsHashEntered[_ipfsHash] = true;
+    }
+ 
+ }
