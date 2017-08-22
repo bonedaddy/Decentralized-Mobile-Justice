7e5573e8f6635dda54659173b05481e96a2c8fd7
Postables <alextrottier2014@gmail.com>
2017-08-22 04:39:23 -0700

    finalcommit

--- data_receive_contract.sol
+++ data_receive_contract.sol
@@ -62,7 +62,7 @@ contract DataReceive is Owner, SafeMath {
         // unused for now, will be used to track credits;
         uint256 credits;
         // each time  a user makes an upload, the key gets upped by 1, with a new value of the ipfs hash
-        bytes32[] ipfsHashes;
+        string[] ipfsHashes;
         bytes32[] ipfsChecksum;
     }
 
@@ -83,7 +83,7 @@ contract DataReceive is Owner, SafeMath {
     mapping (string => string) public ipfshashToChecksum;
 
     // Event to log data entry
-    event DataEntry(address indexed _recorder, string indexed _ipfsHash, string indexed _ipfsChecksum);
+    event DataEntry(address indexed _recorder, string indexed _ipfsHash, bytes32 indexed _ipfsChecksum);
   
     // Logs a token transfer
     event CreditTransfer(address indexed _this, address indexed _to, uint256 _amount);
@@ -107,27 +107,26 @@ contract DataReceive is Owner, SafeMath {
         require(!ipfsHashEntered[_ipfsHash]);
         // makes sure the person calling the function isn't the recorder
         require(_recorder != msg.sender);
+        bytes32 checksum = sha3(_ipfsHash);
         uploaderTracker[_recorder].numContributions += 1; 
         uploaderTracker[_recorder].credits += 1; 
         // updates the `contributions` mapping which points to a struct
-        uploaderTracker[_recorder].ipfsHashes.push(sha3(_ipfsHash)); 
-        uploaderTracker[_recorder].ipfsHashes.push(sha3(_ipfsHash)); 
+        uploaderTracker[_recorder].ipfsHashes.push(_ipfsHash); 
+        uploaderTracker[_recorder].ipfsChecksum.push(checksum); 
         if(!hasUploaded[_recorder]) {
             hasUploaded[_recorder] = true;
         }
         // Notifies blockchain that data was sotred
-        DataEntry(_recorder, _ipfsHash, _ipfsChecksum);
+        DataEntry(_recorder, _ipfsHash, checksum);
     }
 
-    function DataReceive(string _ipfsHash, address _tokenContractAddress) {
-        uploaderTracker[msg.sender] = Uploaders(msg.sender,0, 0);
-        Uploaders storage u = uploaderTracker[msg.sender];
-        u.numContributions += 1;
-        u.credits = 0;
-        string ipfsChecksum = sha3(_ipfsHash); // we do checksum generation on chain
-        u.contributions = Entry({id: u.numContributions, ipfsHash: _ipfsHash, ipfsChecksum: ipfsChecksum});
-        ipfsHashEntered[_ipfsHash] = true;
-        creditBalances[msg.sender] = 1; // temporary
+    function DataReceive(address _tokenContractAddress) {
+        Uploaders memory t;
+        t.person = msg.sender;
+        t.numContributions = 0;
+        t.credits = 0;
+        uploaderTracker[msg.sender] = t;
+        tokenContractAddress = _tokenContractAddress;
     }
 
     function getIpfsHashChecksum(string _ipfsHash) constant returns (string _checksum) {
