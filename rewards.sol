13e9bbac2e1b11710d31d7d3fe8df6e76c90662c
Postables <alextrottier2014@gmail.com>
2017-08-22 08:56:31 -0700

    Started work on rewards contract

---
+++ rewards.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.11;
+
+contract Owner {
+    
+    address public owner;
+
+    function Owner() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) onlyOwner {
+        owner = _newOwner;
+    }
+}
+
+contract SafeMath {
+      function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract Rewards is SafeMath, Owner {
+
+    // used to track credits
+    mapping (address => uint256) public balances;
+
+    function Rewards() {
+        return true;
+    }
+}
