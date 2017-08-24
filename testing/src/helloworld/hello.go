40bcbbe019c2dfcce2257908575c5057148740e7
Postables <alextrottier2014@gmail.com>
2017-08-24 05:28:38 -0700

    Format fixes

--- testing/src/helloworld/hello.go
+++ testing/src/helloworld/hello.go
@@ -1,8 +1,12 @@
 package main
 
 // main tells the go-tool to create an executable command instead of a packge used by other go programs
-import "fmt"
+import (
+	"fmt"
+	"src/stringUtil"
+)
 
 func main() {
 	fmt.Println("hello")
 }
+echo 
